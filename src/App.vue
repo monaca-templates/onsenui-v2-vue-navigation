59262df5c5ff6bb31a9ff36e37b571e6f2a0ca6a
Adam Kozuch <adam@cmatsudas-MacBook-Air.local>
2017-03-01 13:05:43 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-ons-navigator>
+      <div :is="page" v-for="page in pageStack" :page-stack="pageStack"></div>
+    </v-ons-navigator>
+</template>
+
+<script>
+  import page1 from 'Page1';
+  import page2 from 'Page2';
+  export default {
+    methods: {},
+    data() {
+      return {
+        pageStack: [page1]
+      }
+    },
+    components:{page1, page2} 
+  }
+</script>
---
+++ src/Page2.vue
@@ -0,0 +1,19 @@
+<template id="page2">
+  <v-ons-page>
+    <custom-toolbar :pop="pop" :back-label="'Page 1'">Page 2</custom-toolbar>
+    <p style="text-align: center">This is the second page</p>
+  </v-ons-page>
+</template>
+
+<script>
+  import customToolbar from 'customToolbar';
+  export default {
+    methods: {
+      pop() {
+        this.pageStack.pop();
+      } 
+    },
+    props: ['pageStack'],
+    components: { customToolbar }
+  }
+</script>
