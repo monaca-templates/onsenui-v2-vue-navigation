22ea60cd0d50b5c2a23a1eab0ff09e27a3bbb47a
Eric Corson <emccorson@gmail.com>
2019-05-14 16:07:28 +0100

    Add iPhone X support

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import VueOnsen from 'vue-onsenui';
 
-import 'onsenui';
+import ons from 'onsenui';
 // Onsen UI Styling and Icons
 require('onsenui/css-components-src/src/onsen-css-components.css');
 require('onsenui/css/onsenui.css');
 
 import App from './App';
+
+if (ons.platform.isIPhoneX()) {
+  document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
+  document.documentElement.setAttribute('onsflag-iphonex-landscape', '');
+}
 
 Vue.use(VueOnsen);
 
